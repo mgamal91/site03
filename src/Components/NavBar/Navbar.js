46c44d40d73c6b7e070112ccd2f9fdabdd0136e2
m.gamal <miada.gamal@gmail.com>
2021-12-15 21:57:27 +0200

    nav bar done

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,12 +1,56 @@
 import React from "react";
-import { Nav, NavbarContainer, NavLogo } from "./NavbarElements";
-
+import { FaBars } from "react-icons/fa";
+import {
+  MobileIcon,
+  Nav,
+  NavbarContainer,
+  NavBtn,
+  NavBtnLink,
+  NavItem,
+  NavLinks,
+  NavLogo,
+  NavMenu,
+} from "./NavbarElements";
 const Navbar = () => {
   return (
     <>
       <Nav>
         <NavbarContainer>
           <NavLogo to="/">dolla</NavLogo>
+          <MobileIcon>
+            {/* from react icons */}
+            <FaBars />
+          </MobileIcon>
+          {/* ul */}
+          <NavMenu>
+            {/* li */}
+            <NavItem>
+              {/* about is the id you want to scroll to */}
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            {/* end of li */}
+            {/* li */}
+            <NavItem>
+              {/* about is the id you want to scroll to */}
+              <NavLinks to="discovery">Discovery</NavLinks>
+            </NavItem>
+            {/* end of li */}
+            {/* li */}
+            <NavItem>
+              {/* about is the id you want to scroll to */}
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            {/* end of li */}
+            {/* li */}
+            <NavItem>
+              {/* about is the id you want to scroll to */}
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </NavItem>
+            {/* end of li */}
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+          </NavBtn>
         </NavbarContainer>
       </Nav>
     </>
--- src/Components/NavBar/NavbarElements.js
+++ src/Components/NavBar/NavbarElements.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import {Link as LinkRouter} from 'react-router-dom';
-
+import {Link as LinkScroll} from 'react-scroll';
+/* styling file */
 export const Nav = styled.nav`
   background: #000;
   height: 80px;
@@ -36,3 +37,81 @@ export const NavLogo = styled(LinkRouter)`color:#fff;
     font-weight: bold;
     text-decoration: none;
   }   `;
+/* only viewed through smaller screens */
+export const MobileIcon=styled.div`
+display:none;
+@media screen and (max-width:768px)
+{
+  display:block;
+  position:absolute;
+  top:0;
+  right:0;
+  transform: translate(-100%, 60%);
+  font-size:1.8rem;
+  cursor:pointer; 
+  color:#fff;
+}`
+;
+/* wont show this with smaller screens */
+export const NavMenu=styled.ul`
+display:flex;
+align-items:center;
+list-style:none;
+text-align:center;
+margin-right:-22px;
+@media screen and (max-width:768px)
+{
+  display:none; 
+}
+
+`;
+/* same height as nav bar */
+export const NavItem=styled.li`
+height:80px; `;
+
+/* &.active the active class */
+export const NavLinks=styled(LinkScroll)`
+color:#fff;
+display:flex;
+align-items:center;
+text-decoration:none;
+padding:0 1rem;
+height:100%;
+cursor:pointer;
+
+&.active
+{
+  border-bottom:3px solid #01bf71;
+}`;
+
+/* Languages Button */
+
+export const NavBtn=styled.nav`
+display:flex;
+align-items:center;
+
+@media screen and (max-width:768px)
+{
+  display:none;
+}`;
+export const NavBtnLink= styled(LinkRouter)`
+border-radius:50px;
+background:#01bf71;
+white-space:nowrap;
+padding:10px 22px; 
+color:#010606;
+font-size:16px;
+outline:none;
+border:none;
+cursor:pointer;
+transition: all 0.2s ease0in-out;
+text-decoration:none;
+
+&:hover
+{
+  /* time it takes to ease the effect of hover color change */
+  transition:all 0.2s ease-in-out;
+  background:#fff;
+  color:#010686;
+}
+`;
